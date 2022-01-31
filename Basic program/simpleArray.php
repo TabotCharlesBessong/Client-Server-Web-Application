1bdbce0622b8c8495bb208f45ec259d2e4c74835
Charles Bessong Tabot <82298650+TabotCharlesBessong@users.noreply.github.com>
2022-01-31 09:57:31 +0100

    Create simpleArray.php
    Learning arrays in php

---
+++ Basic program/simpleArray.php
@@ -0,0 +1,13 @@
+<?php
+
+    $myArray[0] = "John";
+
+    $myArray[1] = "<strong>PHP</strong>";
+
+    $myArray[2] = 21;
+
+    $myArray[3] = "<strong>JavaScript</strong>";
+
+   echo "$myArray[0] is $myArray[2] and knows $myArray[1] but loves $myArray[3]";
+
+?>
